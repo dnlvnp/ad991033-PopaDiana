22a0a16a4212845af5d99a5fe78bf6ea8c4d7266
dnlvnp <diana.lavinia.p@gmail.com>
2023-04-23 20:18:52 +0000

    update batiments

--- TPs/TP4/app/app.js
+++ TPs/TP4/app/app.js
@@ -42,16 +42,53 @@ map.on("load", () => {
     });
 
 //2. Unités d'évaluation foncière
-//map.addSource('batiments', {
-//  'type': 'geojson',
-//  'data': 'https://services6.arcgis.com/133a00biU9FItiqJ/arcgis/rest/services/batiments3D/FeatureServer/0/query?f=pgeojson&where=1=1&outFields=*',
-//}),
+map.addSource('batiments', {
+  'type': 'geojson',
+  'data': 'https://services6.arcgis.com/133a00biU9FItiqJ/arcgis/rest/services/batiments3D/FeatureServer/0/query?f=pgeojson&where=1=1&outFields=*',
+}),
 
-//  map.addLayer({
-//    'id': 'batiments',
-//    'type': 'fill',
-//    'source': 'batiments'
-//  });//
+  map.addLayer({
+    'id': 'batiments',
+    'type': 'fill',
+    'source': 'batiments',
+    'paint': {
+      'fill-color': [
+        'match',
+        ['get', 'g_type'],
+        'RESIDENTIAL', '#E9ECEF',
+        'EDUCATIONAL', '#343A40',
+        'HOSPITAL','#ADB5BD',
+        'HEALTH SERVICES','#b1a7a6',
+        'RELIGIOUS', '#8e9aaf',
+        'COURTHOUSE', '#E9ECEF',
+        'CITY HALL', '#b1a7a6',
+        'SHOPPING CENTER','#403d39',
+        'COMMERCIAL','#4c5c68',
+        'INDUSTRIAL','#343A40',
+        'GREENHOUSE','#4c5c68',
+        'LODGING FACILIT', '#E9ECEF',
+        'STADIUM', '#4c5c68',
+        'SPORTSPLEX', '#E9ECEF',
+        'GAS STATION', '#343A40',
+        'GAS AND OIL FAC','#4c5c68',
+        'POLICE STATION', '#8e9aaf',
+        'PENAL BUILDING','#ADB5BD',
+        'FIRE STATION','#403d39',
+        'LIBRARY','#4c5c68',
+        'GRAIN ELEVATOR','#b1a7a6',
+        'COMMUNITY CENTER','#343A40',
+        'MUSEUM','#403d39',
+        'MUNICIPAL','#8e9aaf',
+        'GOLF COURSE', '#ADB5BD',
+        'ARMOURY','#E9ECEF',
+        'ARENA','#4c5c68',
+        'BARN/MACHINER','#343A40',
+        'ATTRACTION','#403d39',
+        'OTHER', '#4c5c68',
+        '#FFFFFF',
+      ],
+    }
+  });
 
 //3. Interventions du SIM dans les crimes
 map.addSource('acsim', {
@@ -86,9 +123,6 @@ map.addSource('acsim', {
 function zoomToGeoJSON() {
   map.fitBounds(geojsonExtent(geoJSONcontent));
 }
-
-
-
 
 //document
 //.getElementById('file')
